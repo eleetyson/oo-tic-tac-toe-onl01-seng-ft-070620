e50dc9f8461aca21cc78c4a76c48f767a7013922
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-15 17:55:08 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,57 @@
+require 'pry'
+class TicTacToe
+  
+  WIN_COMBINATIONS = [ 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ]
+
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input)
+    index = input.to_i - 1
+  end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O" ? true : false
+  end
+  
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index) ? true : false
+  end
+  
+  def turn_count
+    @board.count {|i| i == "X" || i == "O"}
+  end
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.chomp
+    input_to_index(input)
+  end
+  
+end
